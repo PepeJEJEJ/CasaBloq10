5f3d2a35a8739f507e80e56d5e713dfa47f7826a
PepeJEJEJ <jah0005@alu.medac.es>
2026-05-14 20:18:18 +0200

    Update App6.java

--- Apuntes/Repaso/App6.java
+++ Apuntes/Repaso/App6.java
@@ -2,6 +2,50 @@ import java.sql.*;
 
 public class App6 {
     public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        // EL MENU
+        do {
+            System.out.println("1. Listado basico");
+            System.out.println("2. Buscar por curso");
+            System.out.println("3. Media por curso");
+            System.out.println("4. Aprobados/Suspensos");
+            System.out.println("5. Mejor estudiante");
+            System.out.println("0. Chau");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    mostrarListadoBasico();
+                    break;
+                case 2:
+                    BuscarCurso();
+                    break;
+                case 3:
+                    MediaCursos();
+                    break;
+                case 4:
+                    AprobSus();
+                    break;
+                case 5:
+                    SpesialOne();
+                    break;
+                case 6:
+                    Matricular();
+                    break;
+                case 7:
+                    actualizarNota();
+                    break;
+                case 8:
+                    DarBaja();
+                    break;
+            }
+
+        } while (opcion != 0);
+    }
+        public static void mostrarListadoBasico() {
         String url = "jdbc:mysql://localhost:3306/instituto";
         String user = "root";
         String password = "12345";
@@ -28,4 +72,5 @@ public class App6 {
             System.out.println("Error desconocido" + e.getMessage());
         }
     }
+    
 }
